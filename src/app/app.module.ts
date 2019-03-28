58c5ddfa4bb922c1db2403f328abc204eb41dea5
Aluno <aluno@eteclencois.com.br>
2019-03-28 12:26:01 -0300

    commit inicial do aulaapi-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import {TableModule} from 'primeng/table';
     AppRoutingModule,
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
+
     
 
   ],
